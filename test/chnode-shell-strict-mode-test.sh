031b17b28919e0436d88e32bed0a6843b9db90bb
Tuomas Kareinen <tkareine@gmail.com>
2018-09-27 21:38:17 +0300

    DRY setting env for subshell, add missing SHELL envar

--- test/chnode-shell-strict-mode-test.sh
+++ test/chnode-shell-strict-mode-test.sh
@@ -3,7 +3,7 @@
 source test/helper.sh
 
 test_list_in_shell_strict_mode() {
-    env -i TERM="$TERM" HOME="$HOME" CHNODE_NODES_DIR="$CHNODE_NODES_DIR" "$SHELL" >/dev/null <<END
+    with_shell >/dev/null <<END
 set -euo pipefail
 source chnode.sh
 chnode
@@ -12,7 +12,7 @@ END
 }
 
 test_use_in_shell_strict_mode() {
-    env -i TERM="$TERM" HOME="$HOME" CHNODE_NODES_DIR="$CHNODE_NODES_DIR" "$SHELL" >/dev/null <<END
+    with_shell >/dev/null <<END
 set -euo pipefail
 source chnode.sh
 chnode node-8
@@ -22,12 +22,21 @@ END
 }
 
 test_reset_in_shell_strict_mode() {
-    env -i TERM="$TERM" HOME="$HOME" CHNODE_NODES_DIR="$CHNODE_NODES_DIR" "$SHELL" >/dev/null <<END
+    with_shell >/dev/null <<END
 set -euo pipefail
 source chnode.sh
 chnode reset
 END
     assertEquals 0 $?
+}
+
+with_shell() {
+    env -i \
+        TERM="$TERM" \
+        HOME="$HOME" \
+        CHNODE_NODES_DIR="$CHNODE_NODES_DIR" \
+        SHELL="$SHELL" \
+        "$SHELL"
 }
 
 source "$SHUNIT2"
--- test/runner.sh
+++ test/runner.sh
@@ -7,7 +7,12 @@ echo "Using SHELL=$SHELL"
 error=0
 
 for test_file in "$@"; do
-    env -i TERM="$TERM" HOME="$HOME" "$SHELL" "$test_file" || (( error += 1 ))
+    env -i \
+        TERM="$TERM" \
+        HOME="$HOME" \
+        SHELL="$SHELL" \
+        "$SHELL" "$test_file" \
+        || (( error += 1 ))
 done
 
 exit "$error"
