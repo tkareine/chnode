bdd308b8b78080073b9ad7af3eedc79803d5648f
Tuomas Kareinen <tkareine@gmail.com>
2018-09-28 21:16:04 +0300

    Test env after sourcing

---
+++ test/chnode-after-source-test.sh
@@ -0,0 +1,38 @@
+# -*- sh-shell: bash; -*-
+
+source test/helper.sh
+
+test_populates_nodes_var() {
+    local expected_paths=(
+        "$CHNODE_NODES_DIR/iojs-3.3.1" \
+        "$CHNODE_NODES_DIR/node-10.11.0" \
+        "$CHNODE_NODES_DIR/node-8.1.0" \
+        "$CHNODE_NODES_DIR/node-9.11.2" \
+        "$CHNODE_NODES_DIR/node-9.11.2-rc1"
+    )
+    assertEquals "${expected_paths[*]}" "${CHNODE_NODES[*]}"
+}
+
+test_nodes_root_var_is_null() {
+    assertNull "\$CHNODE_ROOT must be null" "$CHNODE_ROOT"
+}
+
+test_chnode_list() {
+    local expected_output
+    expected_output=$(cat <<END
+   iojs-3.3.1
+   node-10.11.0
+   node-8.1.0
+   node-9.11.2
+   node-9.11.2-rc1
+END
+)
+
+    local actual_output
+    actual_output=$(chnode)
+
+    assertEquals 0 $?
+    assertEquals "$expected_output" "$actual_output"
+}
+
+SHUNIT_PARENT=$0 source "$SHUNIT2"
--- test/chnode-test.sh
+++ test/chnode-test.sh
@@ -7,25 +7,6 @@ setUp() {
     chnode reset
 }
 
-test_chnode_list() {
-    local expected_output
-    expected_output=$(cat <<END
-   iojs-3.3.1
-   node-10.11.0
-   node-8.1.0
-   node-9.11.2
-   node-9.11.2-rc1
-END
-)
-
-    local actual_output
-    actual_output=$(chnode)
-
-    assertEquals 0 $?
-    assertEquals "$expected_output" "$actual_output"
-    assertNull "\$CHNODE_ROOT must be null" "$CHNODE_ROOT"
-}
-
 test_chnode_use_and_reset() {
     chnode node-8
 
