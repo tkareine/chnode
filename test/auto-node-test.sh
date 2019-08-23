eb9c2c49d98a6708c264e59e998ffe6c935ce784
Tuomas Kareinen <tkareine@gmail.com>
2019-08-23 23:39:32 +0300

    Cleanup nodes fixture dir, use test dirs

--- test/auto-node-test.sh
+++ test/auto-node-test.sh
@@ -1,7 +1,6 @@
 # -*- sh-shell: bash; -*-
 
 source test/setup-shunit2.sh
-source test/setup-default-chnode.sh
 source test/setup-default-auto.sh
 
 setUp() {
@@ -51,43 +50,43 @@ test_chnode_auto_loaded_twice() {
 }
 
 test_chnode_auto_enter_project_dir() {
-    cd "$CHNODE_NODES_AUTO_DIR" && echo "node-8.1.0" > .node-version
-    cd "$CHNODE_NODES_AUTO_DIR" && chnode_auto
+    cd "$__CHNODE_AUTO_DIR" && echo "node-8.1.0" > .node-version
+    cd "$__CHNODE_AUTO_DIR" && chnode_auto
 
     assertEquals "did not switch Node when entering a versioned directory" \
                  "$test_node_root" "$CHNODE_ROOT"
 }
 
 test_chnode_auto_enter_subdir_directly() {
-    cd "$CHNODE_NODES_AUTO_DIR" && echo "node-8.1.0" > .node-version
-    cd "$CHNODE_NODES_AUTO_DIR" && chnode_auto
-    cd "$CHNODE_NODES_AUTO_DIR/sub_dir" && chnode_auto
+    cd "$__CHNODE_AUTO_DIR" && echo "node-8.1.0" > .node-version
+    cd "$__CHNODE_AUTO_DIR" && chnode_auto
+    cd "$__CHNODE_AUTO_DIR/sub_dir" && chnode_auto
 
     assertEquals "did not switch Node when directly entering a sub-directory of a versioned directory" \
                  "$test_node_root" "$CHNODE_ROOT"
 }
 
 test_chnode_auto_enter_subdir() {
-    cd "$CHNODE_NODES_AUTO_DIR" && echo "node-8.1.0" > .node-version
-    cd "$CHNODE_NODES_AUTO_DIR" && chnode_auto
-    cd sub_dir                  && chnode_auto
+    cd "$__CHNODE_AUTO_DIR" && echo "node-8.1.0" > .node-version
+    cd "$__CHNODE_AUTO_DIR" && chnode_auto
+    cd sub_dir && chnode_auto
 
     assertEquals "did not keep the current Node when entering a sub-dir" \
                  "$test_node_root" "$CHNODE_ROOT"
 }
 
 test_chnode_auto_modified_node_version() {
-  cd "$CHNODE_NODES_AUTO_DIR/modified_version" && echo "node-10.11.0" > .node-version
-  cd "$CHNODE_NODES_AUTO_DIR/modified_version" && chnode_auto
-  echo "node-8.1.0" > .node-version            && chnode_auto
+  cd "$__CHNODE_AUTO_DIR/modified_version" && echo "node-10.11.0" > .node-version
+  cd "$__CHNODE_AUTO_DIR/modified_version" && chnode_auto
+  echo "node-8.1.0" > .node-version && chnode_auto
 
   assertEquals "did not detect the modified .node-version file" \
                "$test_node_root" "$CHNODE_ROOT"
 }
 
 test_chnode_auto_no_override_manual() {
-  cd "$CHNODE_NODES_AUTO_DIR" && echo "node-8.1.0" > .node-version
-  cd "$CHNODE_NODES_AUTO_DIR" && chnode_auto
+  cd "$__CHNODE_AUTO_DIR" && echo "node-8.1.0" > .node-version
+  cd "$__CHNODE_AUTO_DIR" && chnode_auto
   chnode node-10 && chnode_auto
 
   assertEquals "did not override the Node set in .node-version"\
@@ -95,23 +94,23 @@ test_chnode_auto_no_override_manual() {
 }
 
 test_chnode_auto_leave_project_dir() {
-  cd "$CHNODE_NODES_AUTO_DIR" && echo "node-8.1.0" > .node-version
-  cd "$CHNODE_NODES_AUTO_DIR"    && chnode_auto
-  cd "$CHNODE_NODES_AUTO_DIR/.." && chnode_auto
+  cd "$__CHNODE_AUTO_DIR" && echo "node-8.1.0" > .node-version
+  cd "$__CHNODE_AUTO_DIR" && chnode_auto
+  cd "$__CHNODE_AUTO_DIR/.." && chnode_auto
 
   assertNull "did not reset the Node when leaving a versioned directory" \
              "$CHNODE_ROOT"
 }
 
 test_chnode_auto_invalid_ruby_version() {
-  cd "$CHNODE_NODES_AUTO_DIR" && echo "node-8.1.0" > .node-version
-  cd "$CHNODE_NODES_AUTO_DIR/bad" && echo "foo" > .node-version
+  cd "$__CHNODE_AUTO_DIR" && echo "node-8.1.0" > .node-version
+  cd "$__CHNODE_AUTO_DIR/bad" && echo "foo" > .node-version
   local expected_auto_version
 
-  expected_auto_version="$(cat "$CHNODE_NODES_AUTO_DIR"/bad/.node-version)"
+  expected_auto_version="$(cat "$__CHNODE_AUTO_DIR"/bad/.node-version)"
 
-  cd "$CHNODE_NODES_AUTO_DIR" && chnode_auto
-  cd bad/                     && chnode_auto 2>/dev/null
+  cd "$__CHNODE_AUTO_DIR" && chnode_auto
+  cd bad && chnode_auto 2>/dev/null
 
   assertEquals "did not keep the current Node when loading an unknown version" \
                "$test_node_root" "$CHNODE_ROOT"
--- test/setup-default-auto.sh
+++ test/setup-default-auto.sh
@@ -2,10 +2,15 @@
 
 source support/fixture.sh
 
-fixture_make_default_auto_dir
-CHNODE_NODES_AUTO_DIR=$__FIXTURE_DEFAULT_AUTO_DIR
-fixture_make_default_auto_test_dirs "$CHNODE_NODES_AUTO_DIR"
+fixture_make_default_dir
+CHNODE_NODES_DIR=$__FIXTURE_DEFAULT_DIR
+fixture_make_default_nodes "$CHNODE_NODES_DIR"
 
+fixture_make_default_auto_dir
+__CHNODE_AUTO_DIR=$__FIXTURE_DEFAULT_AUTO_DIR
+fixture_make_default_auto_test_dirs "$__CHNODE_AUTO_DIR"
+
+source chnode.sh
 source auto.sh
 
 # shellcheck disable=SC2034
@@ -15,4 +20,5 @@ export test_node_root="$CHNODE_NODES_DIR/node-8.1.0"
 
 oneTimeTearDown() {
     fixture_delete_default_auto_dir
+    fixture_delete_default_dir
 }
