58f1d13803c75dc1363e5b845450804121d72dc8
Tuomas Kareinen <tkareine@gmail.com>
2019-08-27 22:24:17 +0300

    Allow easier changing of selected node version in a benchmark

--- benchmark/auto-cd-dirs-bm.sh
+++ benchmark/auto-cd-dirs-bm.sh
@@ -1,17 +1,19 @@
 # -*- sh-shell: bash; -*-
 
-echo "(${r:-1}/$RUNS) chnode_auto selecting and deselecting node version by cd dirs"
+NODE_VERSION='node-8.1.0'
+
+echo "(${r:-1}/$RUNS) chnode_auto selecting and deselecting $NODE_VERSION by cd dirs"
 
 source chnode.sh
 source auto.sh
 
-mkdir -p "$__FIXTURE_AUTO_DIR"/node-10
-echo node-10.11.0 >"$__FIXTURE_AUTO_DIR"/node-10/.node-version
+mkdir -p "$__FIXTURE_AUTO_DIR"/project
+echo "$NODE_VERSION" >"$__FIXTURE_AUTO_DIR"/project/.node-version
 
 time (
     for (( i=0; i < N; i+=1 )); do
         # shellcheck disable=SC2164
-        cd "$__FIXTURE_AUTO_DIR"/node-10
+        cd "$__FIXTURE_AUTO_DIR"/project
         chnode_auto
         # shellcheck disable=SC2103
         cd ..
